f7c76090d664cd4a3ef33853e792f52ebd6d9358
jotasil88 <joaopauloluiz2005@gmail.com>
2025-03-14 19:14:43 -0300

    first class Position

---
+++ src/applicationLayer/Program.java
@@ -0,0 +1,13 @@
+package applicationLayer;
+
+import boardLayer.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Position p = new Position(0, 0);
+		
+		System.out.println(p);
+	}
+}
